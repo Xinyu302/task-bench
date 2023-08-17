2cc82b380a63e3ec964584389c4a5effe0e544e6
yxy <summerhh54@gmail.com>
2023-08-17 23:20:51 +0800

    add -I -L for core.cc

--- core/make_blas.mk
+++ core/make_blas.mk
@@ -5,8 +5,8 @@ ifndef MKLROOT
 $(error MKLROOT variable is not defined, aborting build)
 endif
 CFLAGS		+= -DMKL_ILP64 -m64 -I${MKLROOT}/include -DUSE_BLAS_KERNEL
-LDFLAGS		+= -L${MKLROOT}/lib/intel64 -Wl,--no-as-needed -lmkl_intel_ilp64 -lmkl_sequential -lmkl_core -lpthread -lm -ldl
+LDFLAGS		+= -L${MKLROOT}/lib/intel64 -L/usr/local/cuda-11.8/lib64 -Wl,--no-as-needed -lmkl_intel_ilp64 -lmkl_sequential -lmkl_core -lpthread -lm -ldl
 CXXFLAGS	+= -DMKL_ILP64 -m64 -I${MKLROOT}/include -DUSE_BLAS_KERNEL
 CC_FLAGS	+= -DMKL_ILP64 -m64 -I${MKLROOT}/include -DUSE_BLAS_KERNEL
-LD_FLAGS	+= -L${MKLROOT}/lib/intel64 -Wl,--no-as-needed -lmkl_intel_ilp64 -lmkl_sequential -lmkl_core -lpthread -lm -ldl
+LD_FLAGS	+= -L${MKLROOT}/lib/intel64 -L/usr/local/cuda-11.8/lib64 -Wl,--no-as-needed -lmkl_intel_ilp64 -lmkl_sequential -lmkl_core -lpthread -lm -ldl
 endif
